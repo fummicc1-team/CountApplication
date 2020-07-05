21eaf133f56b7a1da3af177578070b442e1f7fc7
fummicc1 <fumiya.tennis1@gmail.com>
2020-07-05 12:06:08 +0900

    update

--- CountApplication/ViewController.swift
+++ CountApplication/ViewController.swift
@@ -6,16 +6,13 @@ class ViewController: UIViewController {
     
     // MARK: Step2. カウントしている数字を格納しておく変数を宣言しよう。
     
-    // MARK: Step3. viewDidLoadって何だろう？下のコメントに知っていることを書いてみよう。
-    /*　[ここにviewDidLoadの説明を書いてね]
-     
-     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    // MARK: Step4. plusメソッドはいつ呼ばれるのかな？Storyboardを確認しながら考えてみよう。
+    // MARK: Step3. plusメソッドはいつ呼ばれるのかな？Storyboardを確認しながら考えてみよう。
     @IBAction func plus() {
-        // MARK: Step5. ここにはどんな処理を書けばいいのだろう？ネットで調べながら自分で書いてみよう。
+        // MARK: Step4. ここにはどんな処理を書けばいいのだろう？ネットで調べながら自分で書いてみよう。
     }
 }
