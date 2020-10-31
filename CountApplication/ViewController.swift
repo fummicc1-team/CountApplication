dc36f7b637380ee605375ec4518db1bb7ea1f782
fummicc1 <fumiya.tennis1@gmail.com>
2020-10-31 14:25:10 +0900

    Update Mark comments.

--- CountApplication/ViewController.swift
+++ CountApplication/ViewController.swift
@@ -1,18 +1,25 @@
 import UIKit
 
+// MARK: Step0. Main.storyboardで画面上に必要な部品を設置して、レイアウトも整えよう！
 class ViewController: UIViewController {
     
-    // MARK: Step1. Main.storyboardからUILabelを初期化するコード
+    // MARK: Step1. Main.storyboardに配置されたUILabelをViewController.swiftから使用できるように変数を宣言してみよう
     
-    // MARK: Step2. カウントしている数字を格納しておく変数を宣言しよう。
+    // MARK: Step2. カウントしている数字を数えるためにInt型の変数を宣言してみよう
     
     
+    // MARK: Step3. Step2が終了したら一度関連付けをしてみよう。うまく関連付けができているかな
+    
+    // MARK: ViewDidLoadの説明をしてみよう
+    /*
+     ここにViewDidLoadについて知っていることを書いてみてね
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    // MARK: Step3. plusメソッドはいつ呼ばれるのかな？Storyboardを確認しながら考えてみよう。
+    // MARK: Main.storyboardに配置されたUIButtonがタップされた際の処理をViewController.swift側で実装するために下の行のコードを追加したよ
     @IBAction func plus() {
-        // MARK: Step4. ここにはどんな処理を書けばいいのだろう？ネットで調べながら自分で書いてみよう。
+        // MARK: Step4. Main.storyboardに配置されたUIButtonがタップされた際の処理を書いてみよう
     }
 }
